4ac8f4127b1f9b9081187bb790aeb5eb3bc00cd0
Olivier Pimi <pimium@gmail.com>
2018-09-29 03:16:15 +0200

    update the fuses: 8mhz internal clock

--- main.c
+++ main.c
@@ -57,6 +57,12 @@
 #define TRIAC_GATE_N PD7
 #endif
 
+// define some macros
+#define BAUD 9600                                   // define baud
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)            // set baud rate value for UBRR
+
+
+
 volatile uint8_t counter_interrupt = 0;
 volatile uint8_t counter_max = 80;
 volatile uint8_t armed = 0;
@@ -131,25 +137,100 @@ static inline void init_PCIE_Interrupt(void)
   PORTB &= ~(1 << ZERO_CROSSING); // disable pull-up
 }
 
+
+static inline void adc_init()
+{
+    // AREF = AVcc
+//    ADMUX = (1<<REFS0) | (1<<REFS1);
+
+    // ADC Enable and prescaler of 128
+    // 16000000/128 = 125000
+    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+}
+
+// function to initialize UART
+void uart_init (void)
+{
+    UBRR0H = (BAUDRATE>>8);                      // shift the register right by 8 bits
+    UBRR0L = BAUDRATE;                           // set baud rate
+    UCSR0B= (1<<TXEN0)|(1<<RXEN0);                // enable receiver and transmitter
+    UCSR0C= (1<<UCSZ00)|(1<<UCSZ01);   // 8bit data format
+}
+
+// function to send data
+void uart_transmit (unsigned char data)
+{
+    while (!( UCSR0A & (1<<UDRE0)));                // wait while register is free
+    UDR0 = data;                                   // load data in the register
+}
+
+// function to receive data
+unsigned char uart_recieve (void)
+{
+    while(!(UCSR0A) & (1<<RXC0));                   // wait while data is being received
+    return UDR0;                                   // return 8-bit data
+}
+
+uint16_t adc_read(uint8_t channel)
+{
+    // select the corresponding channel 0~7
+    // ANDing with ’7′ will always keep the value
+    // of ‘channel’ between 0 and 7
+    channel &= 0x07;  // AND operation with 7
+    ADMUX = (ADMUX & 0xF8) | channel; // clears the bottom 3 bits before ORing
+
+    // start single convertion
+    // write ’1′ to ADSC
+    ADCSRA |= (1<<ADSC);
+
+    // wait for conversion to complete
+    // ADSC becomes ’0′ again
+    // till then, run loop continuously
+    while(ADCSRA & (1<<ADSC));
+
+    return (ADC);
+}
+
+int hex[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 int main(void)
 {
+    volatile int j;
   //  _delay_ms(100);
 
-  DDRD |= ((1 << TRIAC_GATE_N) | (1 << TRIAC_GATE_P));
-  //  DDRB |= (1 << PB2);
+//  DDRD |= ((1 << TRIAC_GATE_N) | (1 << TRIAC_GATE_P));
+    DDRB |= (1 << PB2);
   //  PORTB &= ~(1 << PB2);
-
-  init_IRQ0();
-  initTimer0();
+//
+//  init_IRQ0();
+//  initTimer0();
   //  hv5812_init();
+  uart_init();
+  adc_init();
   sei();
 
   //  PORTB |= (1 << PB1);
   // ---- Main Loop ----
 
+    uint16_t value;
   while (1)
   {
-    // PORTD &= ~(1 << TRIAC_GATE_P);
+        value = adc_read(0);
+//    value = 112;
+      uart_transmit(0x30);
+      uart_transmit('x');
+      uart_transmit(hex[(value >> 12) & 0xf]);
+      uart_transmit(hex[(value >> 8) & 0xf]);
+      uart_transmit(hex[(value >> 4) & 0xf]);
+      uart_transmit(hex[(value >> 0) & 0xf]);
+      uart_transmit('\r');
+      uart_transmit('\n');
+      for (int i = 0; i < 0x8000; ++i) {
+
+      }
+      uart_transmit(hex[(j++) & 0xf]);
+      PORTB ^= (1 << PB2);
+
+
   }
 
   return 0;
